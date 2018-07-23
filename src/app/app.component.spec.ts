7f8d620368de67de0f4ff64491ded14eac5c8137
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-07-23 21:23:37 +0200

    fix test; add karma.config

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to travis-ci-example-nodejs-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to tea!');
   }));
 });
